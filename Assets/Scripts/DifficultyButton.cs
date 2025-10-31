5e61111444997d4d4c1f02fbdc5162a560071a61
Akshatha16092001 <akshatha.poojari@indivillage.co>
2025-10-31 15:56:19 +0530

    adding difficulty levels

--- Assets/Scripts/DifficultyButton.cs
+++ Assets/Scripts/DifficultyButton.cs
@@ -7,10 +7,14 @@ using UnityEngine.UI;
 public class DifficultyButton : MonoBehaviour
 {
     private Button button;
+    public int difficulty;
+    private GameManager gameManager;
+    
     // Start is called before the first frame update
     void Start()
     {
         button = GetComponent<Button>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         button.onClick.AddListener(SetDifficulty);
     }
 
@@ -22,5 +26,7 @@ public class DifficultyButton : MonoBehaviour
     void SetDifficulty()
     {
         Debug.Log(button.gameObject.name + "was clicked");
+        gameManager.StartGame(difficulty);
     }
+    
 }
