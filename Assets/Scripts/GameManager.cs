0756733175ca114dbc46f1898e6dd2bb2176dcf4
Akshatha16092001 <akshatha.poojari@indivillage.co>
2025-10-31 15:34:18 +0530

    adding difficulty class

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -53,7 +53,7 @@ public class GameManager : MonoBehaviour
     }
     public void RestartGame()
     {
-        
+         
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
